b804c8321e041748529933d3a28be9e3af6aae36
ShotRep <78008209+ShotRep@users.noreply.github.com>
2021-07-14 17:36:38 -0500

    fetch demo

---
+++ script.js
@@ -0,0 +1,33 @@
+/*
+FETCH API or fetch('url' - 1st property, 2nd optional property)
+lets us a fake api => https://reqres.in/api/users
+get
+post
+put
+*/
+//1.)
+console.log(fetch('https://reqres.in/api/users'))
+//we see this returns a promise so we can use asyc await, .then, and .catch
+
+//2.)
+fetch("https://reqres.in/api/users")
+.then(res => console.log(res))
+//res = the response object.
+//body in out response object contains all the data but we cant see it.
+//we need to convert this to JSON to view.
+
+fetch("https://reqres.in/api/users")
+  .then(res => res.json())
+  .then(data => console.log(data))
+ 
+  //Fetch always succeeds unless there is no network connectivity so .catch doesn't work the way it should.  We have a work around but easiest handled otherwise.
+
+fetch("https://reqres.in/api/users/13")
+  .then((res) => {
+    if (res.ok){
+      console.log('Success')
+    } else {
+      console.log('Not Successful')
+    }
+    })
+    .then(data => console.log(data))
