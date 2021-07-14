f81358750a6eba2f03537d8fbbc82fb25c129674
ShotRep <78008209+ShotRep@users.noreply.github.com>
2021-07-14 17:49:49 -0500

    fetch demo complete with notes

---
+++ README.md
@@ -0,0 +1,14 @@
+<h1 align="center">FETCH API</h1>
+
+<h3 align="center">Learn Fetch API In 6 Minutes</h3>    
+
+<!-- GETTING STARTED -->
+## Getting Started
+Quick runthrough of 
+</br>
+[FETCH API IN 6 MIN VIDEO - URL](https://www.youtube.com/watch?v=cuEtnrL9-H0)
+
+<!-- LICENSE -->
+## License
+
+Distributed under the MIT License. See `LICENSE` for more information.
--- script.js
+++ script.js
@@ -1,33 +1,47 @@
 /*
 FETCH API or fetch('url' - 1st property, 2nd optional property)
 lets us a fake api => https://reqres.in/api/users
-get
-post
-put
+optional section(get method, post method, put method)
+
 */
 //1.)
-console.log(fetch('https://reqres.in/api/users'))
+console.log(fetch("https://reqres.in/api/users"))
 //we see this returns a promise so we can use asyc await, .then, and .catch
 
 //2.)
-fetch("https://reqres.in/api/users")
-.then(res => console.log(res))
+fetch("https://reqres.in/api/users").then((res) => console.log(res))
 //res = the response object.
 //body in out response object contains all the data but we cant see it.
 //we need to convert this to JSON to view.
 
 fetch("https://reqres.in/api/users")
-  .then(res => res.json())
-  .then(data => console.log(data))
- 
-  //Fetch always succeeds unless there is no network connectivity so .catch doesn't work the way it should.  We have a work around but easiest handled otherwise.
+  .then((res) => res.json())
+  .then((data) => console.log(data))
+
+//Fetch always succeeds unless there is no network connectivity so .catch doesn't work the way it should.  We have a work around but easiest handled otherwise.
 
 fetch("https://reqres.in/api/users/13")
   .then((res) => {
-    if (res.ok){
-      console.log('Success')
+    if (res.ok) {
+      console.log("Success")
     } else {
-      console.log('Not Successful')
+      console.log("Not Successful")
     }
-    })
-    .then(data => console.log(data))
+  })
+  .then((data) => console.log(data))
+
+//Lets POST a new user by using methods in our optional fetch.
+//send the data using JSON
+fetch("https://reqres.in/api/users/", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
+    name: "Ragnor",
+  }),
+})
+  .then((res) => {
+    return res.json()
+  })
+  .then((data) => console.log(data))
